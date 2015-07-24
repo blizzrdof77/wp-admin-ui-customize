4338d17f432729bd23d0c9df67b8a9a95ecf9fdd
gqevu6bsiz <gqevu6bsiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-24 04:27:13 +0000

    * Fixed: Empty metaboxes load.
    git-svn-id: https://plugins.svn.wordpress.org/wp-admin-ui-customize/trunk@1205457 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-admin-ui-customize.php
+++ wp-admin-ui-customize.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: WP Admin UI Customize
 Description: An excellent plugin to customize the management screens.
-Plugin URI: http://wpadminuicustomize.com/?utm_source=use_plugin&utm_medium=list&utm_content=wauc&utm_campaign=1_5_3
-Version: 1.5.3
+Plugin URI: http://wpadminuicustomize.com/?utm_source=use_plugin&utm_medium=list&utm_content=wauc&utm_campaign=1_5_4
+Version: 1.5.4
 Author: gqevu6bsiz
-Author URI: http://gqevu6bsiz.chicappa.jp/?utm_source=use_plugin&utm_medium=list&utm_content=wauc&utm_campaign=1_5_3
+Author URI: http://gqevu6bsiz.chicappa.jp/?utm_source=use_plugin&utm_medium=list&utm_content=wauc&utm_campaign=1_5_4
 Text Domain: wauc
 Domain Path: /languages
 */
@@ -58,7 +58,7 @@ class WP_Admin_UI_Customize
 
 
 	function __construct() {
-		$this->Ver = '1.5.3';
+		$this->Ver = '1.5.4';
 		$this->Name = 'WP Admin UI Customize';
 		$this->Dir = plugin_dir_path( __FILE__ );
 		$this->Url = plugin_dir_url( __FILE__ );
@@ -713,8 +713,12 @@ class WP_Admin_UI_Customize
 						if( is_array( $box ) ) {
 							
 							foreach( $box as $metabox_id => $metabox_detail ) {
+								
+								if( !empty( $metabox_detail ) ) {
 
-								$regist_meta_boxes['metaboxes'][$post_type][$context][$priority][$metabox_id] = strip_tags( $metabox_detail['title'] );
+									$regist_meta_boxes['metaboxes'][$post_type][$context][$priority][$metabox_id] = strip_tags( $metabox_detail['title'] );
+									
+								}
 
 							}
 
